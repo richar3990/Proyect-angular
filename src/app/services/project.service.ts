359117313468b218fd77275bb915296e7fef68ca
Richar Meza <richar3.990@gmail.com>
2021-03-19 19:59:44 -0300

    agregado servicio subida de imagen

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -7,11 +7,21 @@ import { Global } from './global';
 export class ProjectService {
   public url: string;
   constructor(
+    // tslint:disable-next-line:variable-name
     private _http: HttpClient
   ) {
     this.url = Global.url;
   }
   testService(): any{
     return 'Probando el servicio de Angular';
+  }
+  saveProject(project: Project): Observable<any>{
+    const params = JSON.stringify(project);
+    const headers = new HttpHeaders().set('Content-type', 'application/json');
+    return this._http.post(this.url + 'save-project', params,{ headers: headers});
+  }
+  getProjects(): Observable<any>{
+    const headers = new HttpHeaders().set('Content-Type', 'aplication/json');
+    return this._http.get(this.url + 'projects', { headers: headers});
   }
 }
