43bef550005574c9d4d1bae8cee8b4fb54773833
Richar Meza <richar3.990@gmail.com>
2021-03-19 20:45:17 -0300

    agregado servicio

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+import { Global } from '../../services/global';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private _projectService: ProjectService
+  ) {}
   ngOnInit(): void {
+    this.getProjects();
   }
+  getProjects(): any{
+    this._projectService.getProjects().subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        console.log(error as any);
+      }
+    );
+}
 
 }
